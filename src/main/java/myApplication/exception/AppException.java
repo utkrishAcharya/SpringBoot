63572db27db4b429df2938be462e2f5342b508d4
utkrishAcharya <acharyautkrish14@gmail.com>
2026-01-30 13:31:01 +0545

    Update AppException.java

--- src/main/java/myApplication/exception/AppException.java
+++ src/main/java/myApplication/exception/AppException.java
@@ -3,10 +3,12 @@ package myApplication.exception;
 public class AppException extends RuntimeException {
 
     public AppException(String message) {
+        
         super(message);
     }
 
     public AppException(String message, Throwable cause) {
+        
         super(message, cause);
     }
 }
