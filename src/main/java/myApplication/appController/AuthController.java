ab8263bea7d4a6d60470c2f1afc9e1b7684424fa
utkrishAcharya <acharyautkrish14@gmail.com>
2026-01-30 13:29:55 +0545

    Add missing import statement in AuthController

--- src/main/java/myApplication/appController/AuthController.java
+++ src/main/java/myApplication/appController/AuthController.java
@@ -8,6 +8,7 @@ import myApplication.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
