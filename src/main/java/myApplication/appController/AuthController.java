53b934bd6819c30b5d7de76e033c83ed25710f5a
utkrishAcharya <acharyautkrish14@gmail.com>
2026-05-21 07:11:18 +0545

    Update AuthController.java

--- src/main/java/myApplication/appController/AuthController.java
+++ src/main/java/myApplication/appController/AuthController.java
@@ -14,6 +14,7 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+    
 
     @PostMapping("/signup")
     public User signup(@RequestBody SignupRequest signupRequest) {
