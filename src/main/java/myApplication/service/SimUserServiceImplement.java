d3016a80ce8e3fb59edd51ec479c0eb88d408127
utkrishAcharya <acharyautkrish14@gmail.com>
2026-05-29 20:13:31 +0545

    Update SimUserServiceImplement.java

--- src/main/java/myApplication/service/SimUserServiceImplement.java
+++ src/main/java/myApplication/service/SimUserServiceImplement.java
@@ -4,7 +4,6 @@ import myApplication.model.SimUser;
 import myApplication.repository.SimUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
