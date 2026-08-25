b53064b2c901816faa7ba8da238943c182f1a956
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-25 15:23:31 +0545

    Update AuthController.java

--- src/main/java/myApplication/appController/AuthController.java
+++ src/main/java/myApplication/appController/AuthController.java
@@ -19,8 +19,7 @@ public class AuthController {
     public User signup(@RequestBody SignupRequest signupRequest) {
         
         
-        
-        return authService.signup(signupRequest);
+         return authService.signup(signupRequest);
         
     }
     
